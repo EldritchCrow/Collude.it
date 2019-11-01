49c322539195d4748853d3c85a368b1300503ac6
DESKTOP-4S7ACB7\Jonah <mudsej@rpi.edu>
2019-11-01 01:33:45 -0400

    Let's try this again, committing css and markup updates for homepage.

--- HomePage/homePage.js
+++ HomePage/homePage.js
@@ -5,4 +5,23 @@ $(document).ready( function() {
             $(".chatBox").append( "<div class = \"message\">"+ message + "</div>" );
         }
     });
+
+    $("#timeIcon").click( function() {
+      $("#locationIcon").css("background", "#666");
+      $("#calendarIcon").css("background", "#666");
+      $("#timeIcon").css("background", "grey");
+      $("#sideBarContent").text = "time preferences tab";
+    });
+    $("#locationIcon").click( function() {
+      $("#timeIcon").css("background", "#666");
+      $("#calendarIcon").css("background", "#666");
+      $("#locationIcon").css("background", "grey");
+      $("#sideBarContent").text = "location preferences tab";
+    });
+    $("#calendarIcon").click( function() {
+      $("#locationIcon").css("background", "#666");
+      $("#timeIcon").css("background", "#666");
+      $("#calendarIcon").css("background", "grey");
+      $("#sideBarContent").text = "upcoming meetings";
+    });
 })
