9f68c1f48f4e46b91baac8ce7382f3ab1ba7eaec
DESKTOP-4S7ACB7\Jonah <mudsej@rpi.edu>
2019-11-01 00:50:30 -0400

    Revert "markup and css changes to start making homepage look like the figma hifi"
    This reverts commit 7a7caac961ad950c569988e95b8f5f7613a62785.

--- HomePage/homePage.js
+++ HomePage/homePage.js
@@ -5,23 +5,4 @@ $(document).ready( function() {
             $(".chatBox").append( "<div class = \"message\">"+ message + "</div>" );
         }
     });
-
-    $("#timeIcon").click( function() {
-      $("#locationIcon").css("background", "black");
-      $("#calendarIcon").css("background", "black");
-      $("#timeIcon").css("background", "grey");
-      $("#sideBarContent").text = "time preferences tab";
-    });
-    $("#locationIcon").click( function() {
-      $("#timeIcon").css("background", "black");
-      $("#calendarIcon").css("background", "black");
-      $("#locationIcon").css("background", "grey");
-      $("#sideBarContent").text = "location preferences tab";
-    });
-    $("#calendarIcon").click( function() {
-      $("#locationIcon").css("background", "black");
-      $("#timeIcon").css("background", "black");
-      $("#calendarIcon").css("background", "grey");
-      $("#sideBarContent").text = "upcoming meetings";
-    });
 })
