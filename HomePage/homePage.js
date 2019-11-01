8e7f8c144913d428948bbd1d45e3342d8a6c6231
DESKTOP-4S7ACB7\Jonah <mudsej@rpi.edu>
2019-11-01 12:04:12 -0400

    more css and js changes to make homepage look and act better

--- HomePage/homePage.js
+++ HomePage/homePage.js
@@ -3,25 +3,38 @@ $(document).ready( function() {
         var message = $.trim($("#userMessage").val());
         if (message != "") {
             $(".chatBox").append( "<div class = \"message\">"+ message + "</div>" );
+            $("#userMessage").text("");
         }
     });
+    $(document).on('keypress',function(e) {
+    if(e.which == 13) {
+        var message = $.trim($("#userMessage").val());
+        if (message != "") {
+            $(".chatBox").append( "<div class = \"message\">"+ message + "</div>" );
+        }
+        $("#userMessage").text("");
+    }
+  });
 
     $("#timeIcon").click( function() {
       $("#locationIcon").css("background", "#666");
       $("#calendarIcon").css("background", "#666");
       $("#timeIcon").css("background", "grey");
       $("#sideBarContent").text("time preferences tab");
+      $("#notifBar").text("Notifications");
     });
     $("#locationIcon").click( function() {
       $("#timeIcon").css("background", "#666");
       $("#calendarIcon").css("background", "#666");
       $("#locationIcon").css("background", "grey");
       $("#sideBarContent").text("location preferences tab");
+      $("#notifBar").text("Notifications");
     });
     $("#calendarIcon").click( function() {
       $("#locationIcon").css("background", "#666");
       $("#timeIcon").css("background", "#666");
       $("#calendarIcon").css("background", "grey");
-      $("#sideBarContent").text("upcoming meetings");
+      $("#sideBarContent").text("upcoming meetings tab");
+      $("#notifBar").text("Request a meeting");
     });
 })
