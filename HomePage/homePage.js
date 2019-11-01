a8425b635ed8f2dcc6fa1d83fe5111d0f1cffd3b
DESKTOP-4S7ACB7\Jonah <mudsej@rpi.edu>
2019-11-01 10:51:02 -0400

    javascript update to change sidebar content on tab click

--- HomePage/homePage.js
+++ HomePage/homePage.js
@@ -10,18 +10,18 @@ $(document).ready( function() {
       $("#locationIcon").css("background", "#666");
       $("#calendarIcon").css("background", "#666");
       $("#timeIcon").css("background", "grey");
-      $("#sideBarContent").text = "time preferences tab";
+      $("#sideBarContent").text("time preferences tab");
     });
     $("#locationIcon").click( function() {
       $("#timeIcon").css("background", "#666");
       $("#calendarIcon").css("background", "#666");
       $("#locationIcon").css("background", "grey");
-      $("#sideBarContent").text = "location preferences tab";
+      $("#sideBarContent").text("location preferences tab");
     });
     $("#calendarIcon").click( function() {
       $("#locationIcon").css("background", "#666");
       $("#timeIcon").css("background", "#666");
       $("#calendarIcon").css("background", "grey");
-      $("#sideBarContent").text = "upcoming meetings";
+      $("#sideBarContent").text("upcoming meetings");
     });
 })
