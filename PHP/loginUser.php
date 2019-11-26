0b4e33c490598c326838d0f56cf1b20b71518997
seandheffernan <43757314+seandheffernan@users.noreply.github.com>
2019-11-26 17:13:41 -0500

    getMeetings and loginUser functions implemented

--- PHP/loginUser.php
+++ PHP/loginUser.php
@@ -1,11 +1,13 @@
 <?php
     function loginUser($username, $password) {
-        $sql = "SELECT username FROM users WHERE username = '" . $username "' LIMIT 1;";
+        $sql = "SELECT users.password_hash, users.user_id, groupMembers.group_id FROM users WHERE username = '" . $username "' INNER JOIN groupMembers ON users.user_id = groupMembers.user_id LIMIT 1;";
         $result = $conn->query($sql);
         if($result->num_rows == 1) {
-            $sql = "SELECT password_hash FROM users WHERE password_hash = '" . password_hash($password, PASSWORD_BCRYPT) . "' AND username = '" . $username ';";
-            result = $conn->query($sql);
-            if ($result->num_rows == 1) {
+            $row = mysql_fetch_row($result);
+            if (password_verify($password, $row[0]))
+                session_start();
+                $_SESSION["user_id"] = $row[1];
+                $_SESSION["group_id"] = $row[2];
                 return true;
             }
         }
