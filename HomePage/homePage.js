fdb03bf414dac00bd1c993e86beafb7ab4a6d553
MadSnailDisease <madsnaildisease123@gmail.com>
2019-12-05 02:22:18 -0500

    Makes chat auto-update and autoscroll

--- HomePage/homePage.js
+++ HomePage/homePage.js
@@ -10,14 +10,15 @@ $(document).ready( function() {
 
   function sendMessage() {
     var user_message = $.trim($("#userMessage").val());
+    $("#userMessage").val("");
     $.ajax({
       type: "POST",
       url: "user_functions/sendMessage.php",
       data: {message: user_message},
       dataType: "text",
       success: function(data, status) {
-        alert(status + " : " + data);
-        console.log(data);
+        // alert(status + " : " + data);
+        // console.log(data);
       },
       error: function(data, status) {
         alert(status + " : " + data);
@@ -54,4 +55,30 @@ $(document).ready( function() {
         $(".tabs").css("display", "block");
         $(".arrow").html("&gt;");
     });
-})
+    
+    $(".chatBox").delay(500).animate({scrollTop: $(".chatBox").prop("scrollHeight")}, "slow");
+});
+
+function loadMessageLog() {
+  var old_height = $(".chatBox").prop("scrollHeight");
+  $.ajax({
+    type: "GET",
+    url: "user_functions/loadMessages.php",
+    dataType: "json",
+    success: function(data, status) {
+      var new_text = ""
+      data.data.forEach(function(item, index) {
+        new_text += "<div class='message'>" + item.name + ": " + item.message + "</div>";
+      });
+      $(".chatBox").html(new_text);
+      var new_height = $(".chatBox").prop("scrollHeight");
+      console.log(old_height + " : " + new_height);
+      if(new_height > old_height) {
+        $(".chatBox").animate({scrollTop: new_height}, "slow");
+        console.log("Tried to scroll");
+      }
+    }
+  });
+}
+
+setInterval(loadMessageLog, 1000);
