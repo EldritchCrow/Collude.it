ef5b7632847a30ac007c3721747fccf52f39bd17
seandheffernan <43757314+seandheffernan@users.noreply.github.com>
2019-12-05 18:13:45 -0500

    Moved getTopTimes to user_functions folder and wrote it. Tested and should work.

--- PHP/user_functions/getTopTimes.php
+++ PHP/user_functions/getTopTimes.php
@@ -30,6 +30,30 @@ class Database {
 }
 
 
+function getTopTime(&$times) {
+    $maxWeekday;
+    $maxWeektime = 0;
+
+    $max = 0;
+
+    foreach($times as $x=>$value) {
+        for ($y = 0; $y < 48; $y++) {
+            if ($max < $value[$y]) {
+                $max = $value[$y];
+                $maxWeekday = $x;
+                $maxWeektime = $y;
+                $times[$x] = $value;
+            }
+        }
+    }
+    $value = $times[$maxWeekday];
+    for ($y = $maxWeektime; $y < $maxWeektime+4; $y++) {
+        $times[$maxWeekday][$y] = 0;
+    }
+
+    return array("day" => $maxWeekday, "time" => $maxWeektime);
+}
+
 
 function getTopTimes() {
     $conn = Database::getConnection();
@@ -37,11 +61,10 @@ function getTopTimes() {
                 FROM datetime_prefs
                 JOIN group_members
                     ON group_members.user_id=datetime_prefs.user_id
-                WHERE group_members.group_id ='50573eb468d497764df6e4b6';";
+                WHERE group_members.group_id ='" . $_SESSION["group_id"] . "';";
     if ($result = mysqli_query($conn, $sql)) {
         if ($result->num_rows > 0) {
             $times = array( "Monday"=>array_fill(0, 48, 0), "Tuesday"=>array_fill(0, 48, 0), "Wednesday"=>array_fill(0, 48, 0), "Thursday"=>array_fill(0, 48, 0), "Friday"=>array_fill(0, 48, 0), "Saturday"=>array_fill(0, 48, 0), "Sunday"=>array_fill(0, 48, 0) );
-            $max = 0;
             while ($row = $result->fetch_assoc()) {
                 for ($x = $row["start_time"]; $x < $row["end_time"]; $x+=50) {
                     if ($x%100 == 30) {
@@ -49,22 +72,17 @@ function getTopTimes() {
                     }
                     $index = (int)($x/50);
                     $times[$row["day"]][$index-1]++;
-                    if ($max < $times[$row["day"]][$index-1]) {
-                        $max = $times[$row["day"]][$index-1];
-                    }
                 }
-                echo $row["start_time"] . " " . $row["end_time"] . " " . $row["day"] . "<br>";
             }
-            echo $max . "<br>";
-            foreach($times as $x=>$value) {
-                foreach($value as $info) {
-                    echo "$info ";
-                }
-                echo "<br>";
-            }
+            
+            $time_one = getTopTime($times);
+            $time_two =  getTopTime($times);
+            $time_three = getTopTime($times);
+
+            $topTime = array($time_one, $time_two, $time_three);
         }
     } else {
-        echo "sad";
+        echo "Unable to get top times";
     }
 }
 
