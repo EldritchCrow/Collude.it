16fdb3c5f561f9a554759ea5d2ae2021edca8534
aSehat <47791263+aSehat@users.noreply.github.com>
2019-12-04 20:25:22 -0500

    Notification finally formatted

--- HomePage/homePage.js
+++ HomePage/homePage.js
@@ -55,3 +55,63 @@ $(document).ready( function() {
         $(".arrow").html("&gt;");
     });
 })
+
+
+// ------  Notification banner  ------ //
+
+let notification = {};
+
+(function(notify){
+
+
+  notify.notice = function(content, opts){
+
+    opts = $.extend( true, {
+
+      type: 'primary', //primary, secondary, success, danger, warning, info, light, dark
+      appendType: 'append', //append, prepend
+      closeBtn: false,
+       autoClose: 80000, // If you want auto close 
+      className: '',
+
+    }, opts);
+
+    let $container = $('#alert-container-'+ opts.position);
+    if(!$container.length){
+      $container = $('<div id="alert-container-' + opts.position + '" class="alert-container"></div>');
+      $('body').append($container);
+    }
+
+    let $el = $(`
+      <div class="alert fade alert-${opts.type}" role="alert">${content}</div>
+    `);
+
+    if(opts.autoClose){
+      $el
+        .append(`
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        `)
+        .addClass('alert-dismissible');
+    }
+
+    if(opts.autoClose){
+
+      let t = setTimeout(() => {
+        $el.alert('close');
+      }, opts.autoClose);
+
+    }
+
+    opts.appendType === 'append' ? $container.append($el) : $container.prepend($el);
+
+    setTimeout(() => {
+      $el.addClass('show');
+    }, 50);
+    
+    return;
+
+  };
+
+})(notification);
