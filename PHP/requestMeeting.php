4f9cc9600ef400e94f1abd88a39c5ad6f21c414f
MadSnailDisease <madsnaildisease123@gmail.com>
2019-12-07 19:15:25 -0500

    Made it so that duplicate meetings can not be added

--- PHP/requestMeeting.php
+++ PHP/requestMeeting.php
@@ -10,6 +10,20 @@ function requestMeeting($meeting_time, $meeting_location) {
     $conn = Database::getConnection();
     if (checkSession()) {
         $meeting_id = bin2hex(random_bytes(12));
+        $sql = "SELECT UNIX_TIMESTAMP(m_time) as m_time, m_location
+                    FROM meetings
+                    WHERE group_id='" . $_SESSION["group_id"] . "';";
+        if($result = mysqli_query($conn, $sql)){
+            while($row = $result->fetch_assoc()) {
+                if($row["m_time"] == $meeting_time && $row["m_location"] == $meeting_location) {
+                    return array("success" => true,
+                                "message" => "Meeting is already in the database");
+                }
+            }
+        } else {
+            return array("success" => false,
+                        "message" => "Could not retrieve existing meetings");
+        }
         $sql = "INSERT INTO meetings (meeting_id, group_id, m_time, m_location, confirmed)";
         $sql .= "VALUES ('" . $meeting_id . "', '"
         . $_SESSION["group_id"] . "', FROM_UNIXTIME("
